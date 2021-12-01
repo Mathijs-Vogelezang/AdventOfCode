ec1592489f558ace306e167c995ec6559b5db22a
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2021-12-01 06:27:19 +0100

    Solution of day 1

---
+++ src/main/java/year2021/solutions/Day1.java
@@ -0,0 +1,58 @@
+package year2021.solutions;
+
+import common.Day;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day1 extends Day {
+    private static final boolean isTest = false;
+
+    public Day1() throws IOException {
+        super(isTest);
+    }
+
+    @Override
+    public String part1(List<String> input) {
+        int increased = 0;
+        int[] values = Day.createIntArray(input);
+
+        int previous = values[0];
+
+        for (int i = 1; i < input.size(); i++) {
+            int current = values[i];
+
+            if (current > previous) {
+                increased++;
+            }
+
+            previous = current;
+        }
+        return Integer.toString(increased);
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        int increased = 0;
+
+        int[] values = Day.createIntArray(input);
+
+        int previous = values[0] + values[1] + values[2];
+
+        for (int i = 3; i < values.length; i++) {
+            int current = values[i] + values[i - 1] + values[i - 2];
+
+            if (current > previous) {
+                increased++;
+            }
+
+            previous = current;
+        }
+
+        return Integer.toString(increased);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Day1();
+    }
+}
