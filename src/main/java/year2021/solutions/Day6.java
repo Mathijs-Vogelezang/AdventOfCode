8783633cbe8dcefc4d4551c930d6449d59751bcf
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2021-12-07 14:59:19 +0100

    Improved solution for day 6

--- src/main/java/year2021/solutions/Day6.java
+++ src/main/java/year2021/solutions/Day6.java
@@ -7,28 +7,32 @@ import java.util.*;
 
 public class Day6 extends Day {
     private static final boolean isTest = false;
+    long[] daysAndFishes;
 
     public Day6() throws IOException {
         super(isTest);
     }
 
     @Override
+    public void setup() {
+        daysAndFishes = new long[9];
+
+        for (String amountOfDays : input.get(0).split(",")) {
+            daysAndFishes[Integer.parseInt(amountOfDays)]++;
+        }
+    }
+
+    @Override
     public String part1() {
-        return Long.toString(calculateFish(80, input.get(0)));
+        return Long.toString(calculateFish(80));
     }
 
     @Override
     public String part2() {
-        return Long.toString(calculateFish(256, input.get(0)));
+        return Long.toString(calculateFish(256));
     }
 
-    private long calculateFish(int days, String input) {
-        long[] daysAndFishes = new long[9];
-
-        for (String amountOfDays : input.split(",")) {
-            daysAndFishes[Integer.parseInt(amountOfDays)]++;
-        }
-
+    private long calculateFish(int days) {
         for (int i = 0; i < days; i++) {
             long zeroDays = daysAndFishes[0];
 
