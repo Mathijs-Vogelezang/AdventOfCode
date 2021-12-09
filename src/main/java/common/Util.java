c5a62aca71805a3a0d3ee865ce28cbe69da38394
Mathijs-Vogelezang <70201164+Mathijs-Vogelezang@users.noreply.github.com>
2021-12-09 12:30:38 +0100

    added containsLetters methods to the Util class

--- src/main/java/common/Util.java
+++ src/main/java/common/Util.java
@@ -1,4 +1,21 @@
 package common;
 
 public class Util {
+    public static boolean containsLetters(String toCheck, String checker) {
+        for (String character : checker.split("")) {
+            if (!toCheck.contains(character)) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean containsLetters(String toCheck, String checker, int minimum) {
+        int amount = 0;
+
+        for (String character : checker.split("")) {
+            if (toCheck.contains(character)) amount++;
+        }
+
+        return amount >= minimum;
+    }
 }
